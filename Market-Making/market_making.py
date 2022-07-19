7c8da07371502f94dc751eb9e2cfac160272b8c6
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-07-19 18:37:31 -0400

    added Alpaca API stuff

--- Market-Making/market_making.py
+++ Market-Making/market_making.py
@@ -1,5 +1,20 @@
 from alpaca_trade_api import Stream
 import config
+import logging
+import requests
+
+# ENABLE LOGGING - options, DEBUG,INFO, WARNING?
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+# Alpaca API
+ALPACA_BASE_URL = 'https://paper-api.alpaca.markets'
+
+HEADERS = {'APCA-API-KEY-ID': config.APCA_API_KEY_ID,
+           'APCA-API-SECRET-KEY': config.APCA_API_SECRET_KEY}
+
 
 client = Stream(config.APCA_API_KEY_ID, config.APCA_API_SECRET_KEY)
 
@@ -12,3 +27,32 @@ trading_pair = 'BTCUSD'
 client.subscribe_crypto_orderbooks(handler, trading_pair)
 
 client.run()
+
+# Post an Order to Alpaca
+
+
+async def post_alpaca_order(symbol, qty, side, type, time_in_force):
+    '''
+    Post an order to Alpaca
+    '''
+    try:
+        order = requests.post(
+            '{0}/v2/orders'.format(ALPACA_BASE_URL), headers=HEADERS, json={
+                'symbol': symbol,
+                'qty': qty,
+                'side': side,
+                'type': type,
+                'time_in_force': time_in_force,
+                'client_order_id': 'market_making_strategy'
+            })
+        logger.info('Alpaca order reply status code: {0}'.format(
+            order.status_code))
+        if order.status_code != 200:
+            logger.info(
+                "Undesirable response from Alpaca! {}".format(order.json()))
+            return False
+    except Exception as e:
+        logger.exception(
+            "There was an issue posting order to Alpaca: {0}".format(e))
+        return False
+    return order.json()

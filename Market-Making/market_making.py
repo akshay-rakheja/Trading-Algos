1ff21dc9553a7cbf0bab43043422d916a7fea813
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-07-25 23:17:39 -0400

    some updates

--- Market-Making/market_making.py
+++ Market-Making/market_making.py
@@ -20,12 +20,51 @@ HEADERS = {'APCA-API-KEY-ID': config.APCA_API_KEY_ID,
 client = CryptoDataStream(config.APCA_API_KEY_ID, config.APCA_API_SECRET_KEY)
 
 
-async def handler(data):
-    logger.info(data)
+async def quote_handler(data):
+    # logger.info(data)
+    # if data.exchange == 'FTXU':
+    #     logger.info('--------Quote on FTXU--------')
+    #     logger.info("Ask Price: {0}".format(data.ask_price))
+    #     logger.info("Bid Price: {0}".format(data.bid_price))
+    #     logger.info("Ask Size: {0}".format(data.ask_size))
+    #     logger.info("Bid Size: {0}".format(data.bid_size))
+    #     logger.info("Exchange: {0}".format(data.exchange))
+    #     logger.info(
+    #         "Bid-Ask Spread: {0}".format(data.ask_price - data.bid_price))
+    if data.exchange == 'CBSE':
+        logger.info('--------Quote on Coinbase---------')
+        logger.info("Ask Price: {0}".format(data.ask_price))
+        logger.info("Bid Price: {0}".format(data.bid_price))
+        logger.info("Ask Size: {0}".format(data.ask_size))
+        logger.info("Bid Size: {0}".format(data.bid_size))
+        logger.info("Exchange: {0}".format(data.exchange))
+        logger.info(
+            "Bid-Ask Spread: {0}".format(data.ask_price - data.bid_price))
+
+
+async def trade_handler(data):
+    # logger.info(data)
+    ftx_price = data.price if data.exchange == 'FTXU' else None
+    cbse_price = data.price if data.exchange == 'CBSE' else None
+
+    if data.exchange == 'FTXU':
+        logger.info('--------Trade on FTXU--------')
+        logger.info('Trade Price: {0}'.format(data.price))
+        logger.info('Trade Size: {0}'.format(data.size))
+
+    if data.exchange == 'CBSE':
+        logger.info('--------Trade on Coinbase---------')
+        logger.info('Trade Price: {0}'.format(data.price))
+        logger.info('Trade Size: {0}'.format(data.size))
+
+    if ftx_price and cbse_price:
+        logger.info('--------Price difference on FTXU and Coinbase---------')
+        logger.info('Price difference: {0}'.format(ftx_price - cbse_price))
 
 trading_pair = 'BTCUSD'
 
-client.subscribe_quotes(handler, trading_pair)
+client.subscribe_quotes(quote_handler, trading_pair)
+# client.subscribe_trades(trade_handler, trading_pair)
 
 client.run()
 
@@ -57,3 +96,11 @@ async def post_alpaca_order(symbol, qty, side, type, time_in_force):
             "There was an issue posting order to Alpaca: {0}".format(e))
         return False
     return order.json()
+
+
+def market_making_conditions():
+    '''
+    Check the market conditions to see what limit orders to place
+    '''
+
+    pass

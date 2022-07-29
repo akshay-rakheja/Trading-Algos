2d40a64bd34d801fc44348a26271f4b58478cc8a
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-07-29 02:44:58 -0400

    added basic funcs

--- Market-Making/market_making.py
+++ Market-Making/market_making.py
@@ -1,8 +1,12 @@
-# from alpaca_trade_api import Stream
-from alpaca.data.live import CryptoDataStream
-import config
+from alpaca.data.historical import CryptoHistoricalDataClient
+from alpaca.data.requests import CryptoBarsRequest, CryptoQuotesRequest, CryptoTradesRequest
+from alpaca.data.timeframe import TimeFrame
+from datetime import datetime, timedelta, date
+from dateutil.relativedelta import relativedelta
+from alpaca.trading.client import TradingClient
 import logging
-import requests
+import config
+import asyncio
 
 # ENABLE LOGGING - options, DEBUG,INFO, WARNING?
 logging.basicConfig(level=logging.INFO,
@@ -17,12 +21,79 @@ HEADERS = {'APCA-API-KEY-ID': config.APCA_API_KEY_ID,
            'APCA-API-SECRET-KEY': config.APCA_API_SECRET_KEY}
 
 
-client = CryptoDataStream(config.APCA_API_KEY_ID, config.APCA_API_SECRET_KEY)
+# stream_client = CryptoDataStream(
+#     config.APCA_API_KEY_ID, config.APCA_API_SECRET_KEY)
+trading_client = TradingClient(
+    config.APCA_API_KEY_ID, config.APCA_API_SECRET_KEY, paper=True)
+
+client = CryptoHistoricalDataClient()
 
 
-global cbse_price
+one_hour_ago = datetime.now() - relativedelta(minute=10)
+print(one_hour_ago)
 
-global ftx_price
+request_params = CryptoBarsRequest(
+    symbol_or_symbols=["ETH/USD"],
+    timeframe=TimeFrame.Minute,
+    start=one_hour_ago
+)
+
+
+async def main():
+    '''
+    Get historical data from Alpaca and calculate RSI and Bollinger Bands.
+    Backtest historical data to determine buy/sell/hold decisions and test performance.
+    After backtesting, plot the results. Then, enter the loop to wait for new data and
+    calculate entry and exit decisions.
+    '''
+    # Log the current balance of the MATIC token in our Alpaca account
+    logger.info('BTC Position on Alpaca: {0}'.format(get_positions()))
+    # Log the current Cash Balance (USD) in our Alpaca account
+    global usd_position
+    usd_position = float(get_account_details()['cash'])
+    logger.info("USD position on Alpaca: {0}".format(usd_position))
+    # Get the historical data from Alpaca for backtesting
+    await get_crypto_bar_data(trading_pair, start_date, today, exchange)
+    # Add bar_data to a CSV for backtrader
+    bar_data.to_csv('bar_data.csv', index=False)
+    # Create and run a Backtest instance
+    await backtest_returns()
+
+    while True:
+        l1 = loop.create_task(get_crypto_bar_data(
+            trading_pair, start_date, today, exchange))
+        # Wait for the tasks to finish
+        await asyncio.wait([l1])
+        # Check if any trading condition is met
+        await check_condition()
+        # Wait for the a certain amount of time between each bar request
+        await asyncio.sleep(waitTime)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
+
+bars_df = client.get_crypto_bars(request_params).df
+
+
+max_high = bars_df['high'].max()
+min_low = bars_df['low'].min()
+mean_vwap = bars_df['vwap'].mean()
+mean_close = bars_df['close'].mean()
+
+buying_fee = 0.003*min_low
+selling_fee = 0.003*max_high
+
+print("Total fees: {0}".format(buying_fee + selling_fee))
+
+diff = max_high - min_low
+print("Spread: {0}".format(diff))
+
+print("Mean Close: {0}".format(mean_close))
+print("Mean VWAP: {0}".format(mean_vwap))
+print("Max High: ", max_high)
+print("Min Low: ", min_low)
 
 
 async def quote_handler(data):
@@ -66,14 +137,11 @@ async def trade_handler(data):
         logger.info('--------Price difference on FTXU and Coinbase---------')
         logger.info('Price difference: {0}'.format(ftx_price - cbse_price))
 
-trading_pair = 'BTCUSD'
+trading_pair = 'BTC/USD'
 
-client.subscribe_quotes(quote_handler, trading_pair)
-client.subscribe_trades(trade_handler, trading_pair)
 
-client.run()
-
-# Post an Order to Alpaca
+quote_request = CryptoQuotesRequest(trading_pair)
+print(quote_request)
 
 
 async def submit_order(symbol, qty, side, type, time_in_force):
@@ -115,24 +183,5 @@ def market_making_conditions():
     - if ftx trade price is outside the average of highs and lows of cbse, close all positions until the price is within the average range
 
     '''
-
-    if global cbse_price and global ftx_price:
-        if cbse_price > ftx_price:
-            logger.info('--------Market conditions met---------')
-            logger.info('Buy order placed on FTXU')
-            logger.info('Sell order placed on Coinbase')
-            logger.info('--------Market conditions met---------')
-            return True
-        elif cbse_price < ftx_price:
-            logger.info('--------Market conditions met---------')
-            logger.info('Sell order placed on FTXU')
-            logger.info('Buy order placed on Coinbase')
-            logger.info('--------Market conditions met---------')
-            return True
-        else:
-            logger.info('--------Market conditions met---------')
-            logger.info('No orders placed')
-            logger.info('--------Market conditions met---------')
-            return True
 
     pass

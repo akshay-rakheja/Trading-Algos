d0b9e8f7393362578bb95c99d7ed8edb0fa5769b
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-10-20 16:53:39 -0400

    adding logic for ETH-> WETH

--- Shorting/scripts/aave_borrow.py
+++ Shorting/scripts/aave_borrow.py
@@ -1,3 +1,11 @@
+from scripts.helpful_scripts import get_account
+from brownie import interface, config, network
+from scripts.get_weth import get_weth
+
 
 def main():
-    pass
+    account = get_account()
+    erc20 = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth()
+    # pass
--- Shorting/scripts/get_weth.py
+++ Shorting/scripts/get_weth.py
@@ -1,6 +1,17 @@
+from scripts.helpful_scripts import get_account
+from brownie import interface, config, network
+
+
 def main():
-    pass
+    get_weth()
+    # pass
 
 
 def get_weth():
-    pass
+    account = get_account()
+    weth = interface.IWeth(
+        config["networks"][network.show_active()]["weth_token"])
+    txn = weth.deposit({"from": account, "value": 0.1*10**18})
+    txn.wait(1)
+    print('Sent 0.1 ETH to WETH contract')
+    return txn
